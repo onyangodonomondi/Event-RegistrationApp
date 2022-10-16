365a5c8085db8529dc298dbcda0dfe23e45f90c4
onyangodonomondi <onyangodonomondi@gmail.com>
2022-10-16 12:05:02 +0300

    contact form update

--- user/models.py
+++ user/models.py
@@ -33,3 +33,29 @@ class Education(models.Model):
     def __str__(self):
         return self.course
 
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    description = models.TextField()
+    img=models.ImageField(upload_to='images/blog', null= True, blank = True)
+
+    def __str__(self):
+        return self.title
+
+class portfolio(models.Model):
+    title = models.CharField(max_length=100)
+    img=models.ImageField(upload_to='images/portfolio', null= True, blank = True)
+
+    def __str__(self):
+        return self.title
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone=models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    
